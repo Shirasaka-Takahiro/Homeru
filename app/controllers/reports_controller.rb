2a490ab5239817382dd6b874382a353e56cd6abd
Shirasaka-Takahiro <kenshirou913@gmail.com>
2019-12-15 18:46:14 +0000

    created users

---
+++ app/controllers/reports_controller.rb
@@ -0,0 +1,19 @@
+class ReportsController < ApplicationController
+  def show
+  end
+
+  def index
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  root to: 'reports#index'
+
+  get 'reports/show'
+  get 'reports/index'
+  get 'reports/edit'
+  get 'reports/update'
+  get 'reports/create'
+  get 'reports/destroy'
+  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
