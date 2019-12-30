79ec6fb85c2594db8734aa665445ceed283685e2
Shirasaka-Takahiro <kenshirou913@gmail.com>
2019-12-30 16:08:02 +0000

    finished NGword

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,4 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+  require 'obscenity/active_model'
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,7 @@
 class Comment < ApplicationRecord
     belongs_to :user
     belongs_to :report
+    validates :content,  obscenity: { sanitize: true, replacement: "[NGワードです]" }
     validates :content, presence: true
     validates :content, length: { maximum: 60 }
 end
