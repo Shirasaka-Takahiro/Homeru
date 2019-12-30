a423aef283035b9c26fb003667542107ba17d25d
Shirasaka-Takahiro <kenshirou913@gmail.com>
2019-12-30 17:28:51 +0000

    finished validation

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,7 @@
 class Comment < ApplicationRecord
     belongs_to :user
     belongs_to :report
-    validates :content, presence: true
+    validates :content, presence: true, ng_word: true
     validates :content, length: { maximum: 60 }
+    validates :content,  obscenity: { sanitize: true, replacement: "[NGワードです]" }
 end
