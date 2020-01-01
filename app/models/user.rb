823dd0a229e111030b34cb6048d654bac569f5cb
Shirasaka-Takahiro <kenshirou913@gmail.com>
2020-01-01 07:13:10 +0000

    changed auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: %i[facebook twitter], :trackable
+  devise :database_authenticatable, :registerable, :trackable,
+         :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: %i[facebook twitter]
 
   mount_uploader :image, ImageUploader
 
